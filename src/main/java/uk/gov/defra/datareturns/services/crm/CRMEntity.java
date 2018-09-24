b1be0d7239bbf4432445df76794587fc9a1abaee
graham-willis-druid <graham.willis.ea@gmail.com>
2018-09-24 15:40:02 +0100

    Added comments

--- src/main/java/uk/gov/defra/datareturns/services/crm/CRMEntity.java
+++ src/main/java/uk/gov/defra/datareturns/services/crm/CRMEntity.java
@@ -1,5 +1,9 @@
 package uk.gov.defra.datareturns.services.crm;
 
+/**
+ * Associate the CRM result object with a base entity class
+ * @param <E>
+ */
 interface CRMEntity<E> {
     E getBaseEntity();
 }
