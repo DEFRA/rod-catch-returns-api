05151198db4efe42e63e4ff5039fe54f462083d0
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-03-03 11:44:02 +0000

    removed @toString

--- src/main/java/uk/gov/defra/datareturns/services/crm/entity/CrmLicenceResponse.java
+++ src/main/java/uk/gov/defra/datareturns/services/crm/entity/CrmLicenceResponse.java
@@ -3,11 +3,9 @@ package uk.gov.defra.datareturns.services.crm.entity;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class CrmLicenceResponse {
     @JsonAlias("defra_name")
     private String permissionNumber;
--- src/main/java/uk/gov/defra/datareturns/services/crm/entity/CrmResponseEntity.java
+++ src/main/java/uk/gov/defra/datareturns/services/crm/entity/CrmResponseEntity.java
@@ -2,13 +2,11 @@ package uk.gov.defra.datareturns.services.crm.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
-@ToString
 public class CrmResponseEntity {
     private List<CrmLicenceResponse> value;
 }
