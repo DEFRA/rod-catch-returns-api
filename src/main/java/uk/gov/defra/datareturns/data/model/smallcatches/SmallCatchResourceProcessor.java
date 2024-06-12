8e475ee1b72c73611e90f921cd453f80ffdffded
Nabeel Amir <someone@example.com>
2024-06-12 16:27:10 +0100

    initial upgrade of project to Java 21

--- src/main/java/uk/gov/defra/datareturns/data/model/smallcatches/SmallCatchResourceProcessor.java
+++ src/main/java/uk/gov/defra/datareturns/data/model/smallcatches/SmallCatchResourceProcessor.java
@@ -2,9 +2,9 @@ package uk.gov.defra.datareturns.data.model.smallcatches;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.hateoas.LinkBuilder;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.server.LinkBuilder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.stereotype.Component;
 import uk.gov.defra.datareturns.data.model.activities.Activity;
 
@@ -12,13 +12,13 @@ import javax.inject.Inject;
 
 @Component
 @ConditionalOnWebApplication
-public class SmallCatchResourceProcessor implements ResourceProcessor<Resource<SmallCatch>> {
+public class SmallCatchResourceProcessor implements RepresentationModelProcessor<EntityModel<SmallCatch>> {
     @Inject
     private RepositoryRestMvcConfiguration configuration;
 
     @Override
-    public Resource<SmallCatch> process(final Resource<SmallCatch> resource) {
-        final LinkBuilder link = configuration.entityLinks().linkForSingleResource(Activity.class, resource.getContent().getActivity().getId());
+    public EntityModel<SmallCatch> process(final EntityModel<SmallCatch> resource) {
+        final LinkBuilder link = configuration.entityLinks().linkForItemResource(Activity.class, resource.getContent().getActivity().getId());
         resource.add(link.withRel("activityEntity"));
         return resource;
     }
